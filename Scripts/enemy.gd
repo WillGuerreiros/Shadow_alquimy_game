519e78b95abe33800e16d305aa95cdf555f0c91c
WILLIAM NUNES GUERREIROS <williamnunes182@gmail.com>
2024-08-09 18:59:06 -0300

    more enemy behaviour

--- Scripts/enemy.gd
+++ Scripts/enemy.gd
@@ -2,9 +2,10 @@ extends CharacterBody2D
 
 class_name enemy
 
-const SPEED = 300.0
+const SPEED = 100.0
 const JUMP_VELOCITY = -400.0
 
+var target = null
 @export var typeElementEnemy = GameManager.elementoenum.FIRE
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
@@ -15,20 +16,22 @@ func _physics_process(delta):
 	# Add the gravity.
 	if not is_on_floor():
 		velocity.y += gravity * delta
-	
-	
+		move_and_slide()
+	if target:
+		var velocity = global_position.direction_to(target.global_position)
+		move_and_collide(velocity * SPEED * delta)
 	move_and_slide()
 
-
-
-func move_inimigo(posicaoPocao : Vector2):
-	move_and_collide(posicaoPocao)
-	
 
 func _on_area_2d_body_entered(body):
 	print("detectou body entrou na area,tipo:  "+ str(body))
 	if body.is_in_group("pocao"):
 		if typeElementEnemy == 0 and body.currentElemento == Color(2, 0.270588, 0, 1) :
 			print("sao mesmo elemento")
-			move_inimigo(body.get_position())
+			target = body
+
+
+
+func _on_area_2d_body_exited(body):
+	target = null
 
