c8485dc40ffeb52a6c0d398fc09e95a0d9e9ffc9
WILLIAM NUNES GUERREIROS <williamnunes182@gmail.com>
2024-08-11 18:42:31 -0300

    update enemy

--- Scripts/enemy.gd
+++ Scripts/enemy.gd
@@ -15,7 +15,7 @@ var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 func _physics_process(delta):
 	# Add the gravity.
 	if not is_on_floor():
-		velocity.y += gravity * delta
+		velocity.y += gravity/100 * delta
 		move_and_slide()
 	if target:
 		var velocity = global_position.direction_to(target.global_position)
