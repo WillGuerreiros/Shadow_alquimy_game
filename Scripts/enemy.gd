7d219c75f526fe6bf0551533adb0771ce1e463bd
WILLIAM NUNES GUERREIROS <williamnunes182@gmail.com>
2024-08-12 11:48:43 -0300

    camera and enemy

--- Scripts/enemy.gd
+++ Scripts/enemy.gd
@@ -2,26 +2,28 @@ extends CharacterBody2D
 
 class_name enemy
 
-const SPEED = 100.0
-const JUMP_VELOCITY = -400.0
+const SPEED = 200
+const JUMP_VELOCITY = -10
 
 var target = null
 @export var typeElementEnemy = GameManager.elementoenum.FIRE
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
-var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")/10
 
 
 func _physics_process(delta):
 	move_and_slide()
 	# Add the gravity.
 	if not is_on_floor():
-		velocity.y += gravity/100 * delta
+		velocity.y += gravity * delta
 		move_and_slide()
 	if target:
-		var velocity = global_position.direction_to(target.global_position)
-		move_and_collide(velocity * SPEED * delta)
+		segue_target(target,delta)
 
+func segue_target(target, delta):
+	var velocity = global_position.direction_to(target.global_position)
+	move_and_collide(velocity * SPEED * delta)
 
 
 func _on_area_2d_body_entered(body):
